e1ac92fa5d24d10a635fc5469af8517ebb39fdc4
Mark Lewis <thnkloud9@gmail.com>
2018-06-19 16:42:14 +0200

    add db selects and inserts

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import sqlite3
-
+import sys
+from time import gmtime, strftime
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -8,9 +9,9 @@ app = Flask(__name__, static_url_path='/static')
 def hello_world():
     return 'Hello, World!'
 
+
 @app.route('/display/')
 def display():
-   # TODO: select latest new object from the db display table
    con = sqlite3.connect("twam.db")
    con.row_factory = sqlite3.Row
    
@@ -20,21 +21,64 @@ def display():
    items = cur.fetchall();
    return render_template('display.html', items=items)
 
+
 @app.route('/interface/', methods=['POST', 'GET'])
 def interface():
    db = sqlite3.connect("twam.db")
    db.row_factory = sqlite3.Row
 
    if request.method == 'POST':
-       # TODO: select a new object from the db, insert into display table, and push post to tumblr
-       cur = db.cursor()
-       cur.execute("select distinct Noun from Entries")
-       words = cur.fetchall();
+       whereWordString = ""
+       title = ""
+       wordString = request.values.get("words")
+       words = wordString.split(",")
 
-       return render_template('done.html', object=words)
+       # select a new object from the db, insert into display table, and push post to tumblr
+       query = "SELECT * FROM Entries WHERE ("
+       for word in words:
+           title += word + " "
+           query += "Noun like '%" + word + "%' OR "
+
+       query += "Noun like '%default%'" 
+       query += ") ORDER BY RANDOM() LIMIT 1"
+     
+       cur = db.cursor()
+       cur.execute(query);
+       items = cur.fetchall();
+
+       # insert into the display table
+       create = 'create table if not exists Display (Id INT, Title TEXT, Description TEXT, Created TEXT)'
+       cur.execute(create)
+
+       for item in items:
+           insert = 'INSERT INTO Display VALUES ('
+           insert += str(item['Id']) + ','
+           insert += "'" + str(title).strip() + "',"
+           insert += "'" + str(item['Description']) + "',"
+           insert += "'" + str(strftime("%s", gmtime())) + "')"
+
+           sys.stderr.write(insert)
+           cur.execute(insert)
+           db.commit() 
+
+           # TODO: post to tumblr
+
+       return render_template('done.html', items=items, words=words)
   
    cur = db.cursor()
    cur.execute("select distinct Noun from Entries")
    words = cur.fetchall();
 
    return render_template('interface.html', words=words)
+
+
+@app.route('/done/')
+def done():
+   con = sqlite3.connect("twam.db")
+   con.row_factory = sqlite3.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from Display ORDER BY created DESC LIMIT 1")
+   
+   items = cur.fetchall();
+   return render_template('done.html', items=items)
