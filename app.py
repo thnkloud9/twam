5087c58f6abea4e5b97f349cb3e6796e339c881d
Mark Lewis <thnkloud9@gmail.com>
2018-06-19 12:25:14 +0200

    add db selects

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
+
+
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
@@ -7,11 +10,31 @@ def hello_world():
 
 @app.route('/display/')
 def display():
-    # TODO: select latest new object from the db display table
-    return render_template('display.html', name='TODO', desc='ALSO TODO')
+   # TODO: select latest new object from the db display table
+   con = sqlite3.connect("twam.db")
+   con.row_factory = sqlite3.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from Display ORDER BY created DESC LIMIT 1")
+   
+   items = cur.fetchall();
+   return render_template('display.html', items=items)
 
 @app.route('/interface/', methods=['POST', 'GET'])
 def interface():
-    #if request.method == 'POST':
-    # TODO: select a new object from the db, insert into display table, and push post to tumblr
-    return render_template('interface.html', name='TODO')
+   db = sqlite3.connect("twam.db")
+   db.row_factory = sqlite3.Row
+
+   if request.method == 'POST':
+       # TODO: select a new object from the db, insert into display table, and push post to tumblr
+       cur = db.cursor()
+       cur.execute("select distinct Noun from Entries")
+       words = cur.fetchall();
+
+       return render_template('done.html', object=words)
+  
+   cur = db.cursor()
+   cur.execute("select distinct Noun from Entries")
+   words = cur.fetchall();
+
+   return render_template('interface.html', words=words)
